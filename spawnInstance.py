32a2350f694264baf271e80b0a1234bb9de81e98
Matthew Demarest <docdem12@gmail.com>
2013-03-08 12:06:42 -0500

    completed spawner to run SpedeSTEM in temp folder with specified id

--- spawnInstance.py
+++ spawnInstance.py
@@ -22,19 +22,19 @@ TEMPZIP = "tempZip.zip"
 def parseArgs():
     parser = argparse.ArgumentParser(\
         description='A utility program for running a single instance of SpedeSTEM')
-    parser.add_argument("zip", metavar="zipFile", type=str,
+    parser.add_argument("reqFiles", metavar="file", help="user files necessary" +
+        "for running SpedeSTEM" , nargs='+')
+    parser.add_argument("--zip", metavar="zipFile",
                 help="Name of folder process will run in and name of zip file after",
-                nargs=1)
-    parser.add_argument("spedeArgs", metavar="spedeArgs", help="Arguments to SpedeSTEM.py",
-                nargs="+")
+                nargs=1, required=True)
 
-    args = parser.parse_args()
+    args, rest = parser.parse_known_args()
     # debugging argument parsing
     # print args
 
-    return args
+    return args.reqFiles, args.zip[0] + '.zip', rest
 
-def generateChild(ID):
+def generateChild(ID, files):
     try :
         origDir = os.getcwd()
 
@@ -46,33 +46,37 @@ def generateChild(ID):
         srcLoc = os.path.join(origDir, SRC)
         zipLoc = os.path.join(origDir, ID)
 
+        for f in files:
+            if not os.path.isabs(f):
+               path = os.path.join(origDir, f)
+            os.rename(path, os.path.basename(f))
+
         # print orgDir, tempDir, spedeLoc, stemLoc, srcLoc
 
         os.symlink(spedeLoc, SPEDE)
         os.symlink(stemLoc, STEM)
         os.symlink(srcLoc, SRC)
 
-        os.system("ls -l " + tempDir)
     except Exception:
         cleanup(tempDir, origDir)
+        print "Error creating temporary directory"
+        exit()
 
-    return {"tempDir": tempDir, "origDir": origDir, "spedeLoc": spedeLoc, "zipLoc": zipLoc}
+    return {"tempDir": tempDir, "origDir": origDir, "zipLoc": zipLoc}
 
-def captureSpede(spedeArgs, files):
+def captureSpede(spedeArgs, paths):
     call = ["python", SPEDE]
     spedeArgs = call + spedeArgs
     subprocess.call(spedeArgs)
 
-    print 'creating archive'
     zf = zipfile.ZipFile(TEMPZIP, mode='w')
     try:
-        for f in glob.glob(".txt"):
+        for f in glob.glob("*.txt"):
             zf.write(f)
     finally:
         zf.close()
-        os.path.join()
-        os.rename(zf, files["zipLoc"])
-        cleanup(files["tempDir"], files["origDir"])
+        os.rename(TEMPZIP, paths["zipLoc"])
+        cleanup(paths["tempDir"], paths["origDir"])
 
 def cleanup(tempDir, origDir):
     os.chdir(origDir)
@@ -87,11 +91,11 @@ def cleanup(tempDir, origDir):
     os.removedirs(tempDir)
 
 def main():
-    args = parseArgs()
-    files = generateChild(args.zip[0])
+    files, zipFile, args = parseArgs()
+    paths = generateChild(zipFile, files)
 
-    # now in tempdir
-    captureSpede(args.spedeArgs, files)
+    # working directory is now tempdir
+    captureSpede(args, paths)
 
 if __name__ == '__main__':
     main()
